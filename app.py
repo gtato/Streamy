4c10c3070fc2493c9b7dab7a69aa68a138c853e8
Genc <gtato6@gmail.com>
2016-06-15 02:07:43 +0200

    small layout improvements

--- app.py
+++ app.py
@@ -11,8 +11,9 @@ import wx, wx.html
 import sys, re
 import subprocess
 import os.path
+import  wx.lib.scrolledpanel as scrolled
 
-aboutText = """<p>User friendly guy for managing streams and playing them with VLC</p>"""
+aboutText = """<p>User friendly GUI for managing streams and playing them with VLC</p>"""
 
 streams={}
 def LoadStreams():
@@ -67,35 +68,67 @@ class Frame(wx.Frame):
         self.Bind(wx.EVT_MENU, self.OnClose, m_exit)
         menuBar.Append(menu, "&File")
         menu = wx.Menu()
+
+        m_streams = menu.Append(wx.ID_ANY, "&Show streams", "Show the list of stream URLs")
+        self.Bind(wx.EVT_MENU, self.OnShowStreams, m_streams )
+
         m_about = menu.Append(wx.ID_ABOUT, "&About", "Information about this program")
         self.Bind(wx.EVT_MENU, self.OnAbout, m_about)
+
         menuBar.Append(menu, "&Help")
         self.SetMenuBar(menuBar)
-        
-        self.statusbar = self.CreateStatusBar()
 
-        panel = wx.Panel(self)
+
+        topPanel = wx.Panel(self)
         box = wx.BoxSizer(wx.VERTICAL)
-        
-        m_text = wx.StaticText(panel, -1, "Favorite Channels")
+
+        search_btn = wx.SearchCtrl(topPanel, size=(200,-1), style=wx.TE_PROCESS_ENTER)
+        search_btn.ShowSearchButton(1)
+        search_btn.ShowCancelButton(1)
+        search_btn.SetMenu(self.MakeMenu())
+        box.Add(search_btn, 0, wx.EXPAND|wx.ALL, 10)
+
+        # box = wx.FlexGridSizer(rows=4, cols=1, hgap=5, vgap=5)
+        search_panel = scrolled.ScrolledPanel(topPanel, size=(20,50))
+        search_box = wx.BoxSizer(wx.VERTICAL)
+        #box = wx.FlexGridSizer(rows=2, cols=1, hgap=5, vgap=5)
+
+
+        m_text = wx.StaticText(topPanel, -1, "Channels")
         m_text.SetFont(wx.Font(14, wx.SWISS, wx.NORMAL, wx.BOLD))
         m_text.SetSize(m_text.GetBestSize())
         box.Add(m_text, 0, wx.ALL, 10)
-        
-        #m_close = wx.Button(panel, wx.ID_CLOSE, "Close")
-        #m_close.Bind(wx.EVT_BUTTON, self.OnClose)
-        #box.Add(m_close, 0, wx.ALL, 10)
-        
-        # bMap = self.scale_bitmap(wx.Bitmap("images/top_channel_hd.png", wx.BITMAP_TYPE_ANY), 80, 50)
-        # sBitMap = wx.StaticBitmap(panel, -1, bMap, (0, 0), (100, 50))
-        # sBitMap.Bind(wx.EVT_MOUSE_EVENTS, self.OnPlay)
-        #
-        # box.Add(sBitMap, 0, wx.ALL, 10)
-        self.LoadFavorites(panel, box)
+        #box.Add(m_text, 0, wx.EXPAND)
 
-        panel.SetSizer(box)
-        panel.Layout()
 
+
+        search_panel.SetSizer(search_box)
+        search_panel.Layout()
+        search_panel.SetupScrolling()
+
+        #box.Add(search_panel, 0, wx.ALL, 10)
+        box.Add(search_panel,0,wx.EXPAND|wx.ALL,border=10)
+
+        favorite_panel = scrolled.ScrolledPanel(topPanel)
+        favorite_box = wx.BoxSizer(wx.VERTICAL)
+        favorite_panel.Bind(wx.EVT_SIZE, lambda evt, temp=favorite_box: self.OnSize(evt, temp))
+
+        m_text2 = wx.StaticText(topPanel, -1, "Favorite")
+        m_text2.SetFont(wx.Font(14, wx.SWISS, wx.NORMAL, wx.BOLD))
+        m_text2.SetSize(m_text2.GetBestSize())
+        box.Add(m_text2, 0, wx.ALL, 10)
+
+        self.LoadFavorites(favorite_panel, favorite_box)
+
+        favorite_panel.SetSizer(favorite_box)
+        favorite_panel.Layout()
+        favorite_panel.SetupScrolling()
+
+        # box.Add(favorite_panel,0,wx.EXPAND|wx.ALL,border=10)
+        box.Add(favorite_panel, 1, wx.EXPAND|wx.ALL,border=10)
+
+        topPanel.SetSizer(box)
+        topPanel.Layout()
 
     def LoadFavorites(self, panel, box):
         for key in streams:
@@ -104,32 +137,32 @@ class Frame(wx.Frame):
             img_name = 'images/%s.png' % key
             img = img_name if os.path.isfile(img_name) else 'images/no_image.png'
             bMap = self.scale_bitmap(wx.Bitmap(img, wx.BITMAP_TYPE_ANY), 80, 50)
-            sBitMap = wx.StaticBitmap(panel, -1, bMap, (0, 0), (100, 50))
-            sBitMap.Bind(wx.EVT_MOUSE_EVENTS, self.OnPlay)
-            sBitMap.Bind(wx.EVT_MOUSE_EVENTS, lambda evt, temp=key: self.OnPlay(evt, temp))
-            box.Add(sBitMap, 0, wx.ALL, 10)
+            bmp = wx.BitmapButton(panel,-1,bMap)
+            bmp.Bind(wx.EVT_BUTTON, lambda evt, temp=key: self.OnPlay(evt, temp))
+            box.Add(bmp, 0, wx.ALL, 10)
+            # box.Add(sBitMap, 0, wx.ALL, 10)
+            # box.Add(sBitMap, 0, wx.EXPAND)
 
     def OnClose(self, event):
-        dlg = wx.MessageDialog(self, 
-            "Do you really want to close this application?",
-            "Confirm Exit", wx.OK|wx.CANCEL|wx.ICON_QUESTION)
-        #result = dlg.ShowModal()
-        #dlg.Destroy()
-        #if result == wx.ID_OK:
-        #    self.Destroy()
         self.Destroy()
-
 
 
     def OnAbout(self, event):
         dlg = AboutBox()
         dlg.ShowModal()
         dlg.Destroy()  
-    
-    def OnPlay(self, event, stream_id):
-        if event.EventType != 10021:
-            return
 
+    def OnSize(self, event, box):
+        width, height = self.GetClientSizeTuple()
+        pass
+
+    def OnShowStreams(self, event):
+        all = ''
+        for key in streams:
+            all += streams[key]['name'] + ': ' + streams[key]['url'] + '\n\n'
+        print all
+
+    def OnPlay(self, event, stream_id):
         #print streams[stream_id]['url']
         # call("vlc %s" % streams[stream_id]['url'], shell=True)
         p = subprocess.Popen("vlc --fullscreen %s" % streams[stream_id]['url'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -140,6 +173,18 @@ class Frame(wx.Frame):
         image = image.Scale(width, height, wx.IMAGE_QUALITY_HIGH)
         result = wx.BitmapFromImage(image)
         return result
+
+    def MakeMenu(self):
+        menu = wx.Menu()
+        item = menu.Append(-1, "Recent Searches")
+        item.Enable(False)
+        for txt in [ "You can maintain",
+                     "a list of old",
+                     "search strings here",
+                     "and bind EVT_MENU to",
+                     "catch their selections" ]:
+            menu.Append(-1, txt)
+        return menu
 
 app = wx.App(redirect=True)   # Error messages go to popup window
 LoadStreams()
